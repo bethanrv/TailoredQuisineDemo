9bb9edbb9e333c901562981f382272b7285c17ce
Your Name <you@example.com>
2020-12-02 16:56:51 -0500

    added details to manage orders area

--- manage-order.js
+++ manage-order.js
@@ -9,3 +9,22 @@ function closeSendPassContainer(){
   document.getElementById('sendPasswordContainer').style.display = 'none';
   document.getElementById('dim').style.display = 'none';
 }
+
+
+//show manage orders area
+function showManageOrdersArea(){
+  document.getElementById('manageOrdersArea').style.display = 'flex';
+}
+//hide login area
+function hideLoginArea(){
+  document.getElementById('signInArea').style.display = 'none';
+}
+
+
+//login...
+function login(){
+  if(true){
+    hideLoginArea();
+    showManageOrdersArea();
+  }
+}
